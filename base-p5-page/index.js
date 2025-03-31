2cc177f2dbe52daa8318c903aaace5b6af6ecb6c
Eugenio Leal <eugenioleal333@gmail.com>
2025-03-31 14:59:22 -0600

    Basic Responsive p5.js page
    basic page template to start with with each p5 example html+js page

---
+++ base-p5-page/index.js
@@ -0,0 +1,26 @@
+let canvasPadding;
+
+function setup() {
+    canvasPadding = floor(windowWidth*0.1) // 10% padding
+    createCanvas(windowWidth - canvasPadding, windowHeight - canvasPadding);
+    centerCanvas();
+    colorMode(HSB, 360, 100, 100);
+    background(0, 0, 50);
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+    canvasPadding = floor(windowWidth*0.1) // 10% padding
+    resizeCanvas(windowWidth - canvasPadding, windowHeight - canvasPadding);
+    centerCanvas();
+    background(0, 0, 50);
+}
+
+function centerCanvas() {
+    let x = (windowWidth - width) / 2;
+    let y = (windowHeight - height) / 2;
+    select('.p5Canvas').style('position', 'absolute').style('left', `${x}px`).style('top', `${y}px`);
+}
