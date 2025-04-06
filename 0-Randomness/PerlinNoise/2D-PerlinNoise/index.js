8a3b906111c627c6480d97af621e8c19cb1ee81c
Eugenio Leal <eugenioleal333@gmail.com>
2025-04-05 22:15:57 -0600

    Add 2D perlin noise grid vizualization
    vizualize in 2D space a 2D perlin noise grid.
    
    Added a y scrolling animation.
    
    Before adding mouse input listening and animation response to
    up/down/left/right arrows.

---
+++ 0-Randomness/PerlinNoise/2D-PerlinNoise/index.js
@@ -0,0 +1,70 @@
+let canvasPadding;
+let inc = 0.02;
+let start = 0;
+let xoff = 0, yoff = 0;
+
+function setup() {
+    canvasPadding = floor(windowWidth*0.1) // 10% padding
+    createCanvas(windowWidth - canvasPadding, windowHeight - canvasPadding);
+    centerCanvas();
+    colorMode(HSB, 360, 100, 100, 255);
+    background(0, 0, 50);
+    pixelDensity(1);
+}
+
+function draw() {
+    loadPixels();
+    let yoff = start;
+    for(let row = 0; row < height; row++) {
+        let xoff = 0;
+        for(let col = 0; col < width; col++) {
+            let idx = (row * width + col) * 4;
+            let r = noise(xoff, yoff) * 255;
+            pixels[idx+0] = r;
+            pixels[idx+1] = r;
+            pixels[idx+2] = r;
+            pixels[idx+3] = 255;
+            xoff += inc;
+        }
+        yoff += inc;
+    }
+    updatePixels();
+    start += inc;
+    // you can modify the number of wave combinations (octaves) and by what factor does the change in amplitude and frequency
+    // in octave generation (falloff amount).
+    // This is done by the noiseDetail(octaves, falloff_amount) function
+    // reference: https://p5js.org/reference/p5/noiseDetail/
+}
+
+function windowResized() {
+    canvasPadding = floor(windowWidth*0.1) // 10% padding
+    resizeCanvas(windowWidth - canvasPadding, windowHeight - canvasPadding);
+    centerCanvas();
+    background(0, 0, 50);
+}
+
+function centerCanvas() {
+    let x = (windowWidth - width) / 2;
+    let y = (windowHeight - height) / 2;
+    select('.p5Canvas').style('position', 'absolute').style('left', `${x}px`).style('top', `${y}px`);
+}
+
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            console.log('Up arrow pressed');
+            up();
+            break;
+        case 'ArrowDown':
+            down();
+            console.log('Down arrow pressed');
+            break;
+        case 'ArrowLeft':
+            console.log('Left arrow pressed');
+            left();
+            break;
+        case 'ArrowRight':
+            console.log('Right arrow pressed');
+            break;
+    }
+});
